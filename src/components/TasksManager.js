3b627460c785f87f0e471630f3ade96824bdd863
Maciej Zadylak <maciej.zadylak@gmail.com>
2021-09-23 15:48:00 +0200

    new solution

--- src/components/TasksManager.js
+++ src/components/TasksManager.js
@@ -58,10 +58,11 @@ class TasksManager extends React.Component {
     }
 
     async componentDidMount() {
-        const aaaa = await this.api.loadTasks();
+        const tasksList = await this.api.loadTasks();
         this.setState({
-            tasks: aaaa
+            tasks: tasksList
         })
+        this.sortList();
     }
 
     setNewText (e) {
@@ -89,29 +90,86 @@ class TasksManager extends React.Component {
     }
 
     timeMeasure (task) {
-        this.id = setInterval(() => {
-            console.log('aaa');
-            this.setState(state => {
-                const { tasks } = state;
-                const newTasks = tasks.map(t => {
-                    if(t === task) {
-                        const newTask = {...task, time: task.time + 1, isRunning: true};
-                        console.log(newTask);
-                        this.api.updateTask(newTask, task.id);
-                        return newTask;
-                    }
-                    return t;
-                });
-                return {tasks: newTasks};
-            })
-        }, 1000);
+        task.isRunning ? this.stopTimer(task) : this.startTimer(task);
     }
 
     startTimer (task) {
-        
+        this.id = setInterval(() => {
+            this.setNewState(task, true);
+        }, 200);
     }
 
     stopTimer (task) {
+        clearInterval(this.id);
+        this.id = null;
+        this.setNewState(task, false);
+    }
+
+    setNewState (task, type) {
+        this.setState(state => {
+            const newTasks = state.tasks.map(t => {
+                if(t.id === task.id) {
+                    const newTask = {...t, time: t.time + 1, isRunning: type};
+                    this.api.updateTask(newTask, newTask.id);
+                    return newTask;
+                }
+                return t;
+            });
+            return {tasks: newTasks};
+        });
+    }
+    
+    closeTask (task) {
+        clearInterval(this.id);
+        this.id = null;
+        this.setState(state => {
+            const { tasks } = state;
+            const newTasks = tasks.map(t => {
+                if(t.id === task.id) {
+                    const newTask = {...t, isDone: true}
+                    this.api.updateTask(newTask, newTask.id);
+                    return newTask;
+                }
+                return t;
+            }) 
+            return {tasks: newTasks};
+        }, () => {
+            this.sortList();
+        })
+    }
+
+    sortList () {
+        const { tasks } = this.state;
+        const newTaskList = tasks.sort((a, b) => {
+            return a.isDone - b.isDone;
+        })
+        this.setState({
+            tasks: newTaskList,
+        })
+    } 
+
+    removeTask (task) {
+        this.setState(state => {
+            const { tasks } = state;
+            const newTasks = tasks.filter(t => {
+                if(task.id !== t.id) {
+                    return t;
+                }
+                const newTask = {...t, isRemoved: true};
+                this.api.updateTask(newTask, newTask.id);
+            });
+            return {tasks: newTasks};
+        })
+    }
+
+    renderTimer (props) {
+        const { seconds } = props;
+
+        return (
+            `${Math.floor(seconds/(60*60)) >= 10 ? Math.floor(seconds/(60*60)) : `0${Math.floor(seconds/(60*60))}`}h
+            : ${Math.floor(seconds/60) >= 10 ? Math.floor(seconds/60) : `0${Math.floor(seconds/60)}`}m
+            : ${seconds%60 >= 10 ? seconds%60 : `0${seconds%60}`}s`
+        )
     }
 
     renderList = () => {
@@ -119,18 +177,20 @@ class TasksManager extends React.Component {
 
         return(
             tasks.map(task => {
-            const { name, time } = task
-            return (
-                <>
-                <header>{ name }, { time } </header>
-                <footer>
-                    <button onClick={ () => this.timeMeasure(task) }>start/stop</button>
-                    <button>zakończone</button>
-                    <button disabled={ true }>usuń</button>
-                </footer>
-                </>
-            )
-            })
+                if(!task.isRemoved) {
+                const { name, time } = task
+                return (
+                    <>
+                    <header>{ name }, { <this.renderTimer seconds={time}/> } </header>
+                    <footer>
+                        <button onClick={ () => this.timeMeasure(task) } disabled={ task.isDone ? true : false}>start/stop</button>
+                        <button onClick={ () => this.closeTask(task) } disabled={ task.isDone ? true : false}>zakończone</button>
+                        <button onClick={ () => this.removeTask(task) } disabled={ task.isDone ? false : true }>usuń</button>
+                    </footer>
+                    </>
+                )}
+                return null;
+                })
         )
     }
 
